c5036ea801b34afda00e2893f72013d09ebdce43
danyismail <dany_arie@yahoo.com>
2020-07-22 23:12:47 +0700

    using redux on apps

--- src/index.js
+++ src/index.js
@@ -2,16 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 import App from './App';
+// import axios from 'axios';
 import * as serviceWorker from './serviceWorker';
 
+
+const initialState = {
+    user_id: 0,
+    user_name: '',
+    level: 0,
+    name: ''
+}
+
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_USER_DATA': return {
+            ...state,
+            user_id: action.payload.user_id,
+            user_name: action.payload.user_name,
+            level: action.payload.level,
+            name: action.payload.name
+        }
+        default: return state
+    }
+}
+
+const storeRedux = createStore(rootReducer)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={storeRedux}>
+                <App />
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
