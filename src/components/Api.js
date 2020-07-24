74cd9c77b133b7a51f5b4ad8ff3e1ea92fdd3c36
danyismail <dany_arie@yahoo.com>
2020-07-25 03:43:58 +0700

    adding api variable

---
+++ src/components/Api.js
@@ -0,0 +1,14 @@
+
+export const BC_API = 'http://dev.beacukai.go.id:9012'
+export const BC_LOGIN = BC_API + '/login'
+export const BC_SERVER_CHECK_USER = BC_API + '/user'
+export const BC_SERVER_ALL_USER = BC_API + '/get-all-users'
+export const BC_SERVER_LIST = BC_API + '/server'
+export const BC_SERVER_CATEGORY = BC_API + '/category/server'
+export const BC_MASTER_CATEGORY = BC_API + '/category/master'
+export const BC_USER_LOG = BC_API + '/log'
+export const BC_LOGOUT = BC_API + '/logout'
+export const BC_GET_CONFIG = BC_API + '/config'
+export const BC_ADD_USER = BC_API + '/register'
+
+
